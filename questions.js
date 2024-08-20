99441ee3a49bf89ebc5b76c69abd208d72ca6258
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-20 23:38:20 +0000

    Update

--- questions.js
+++ questions.js
@@ -9,5 +9,126 @@ let questions = [
       "yes,and",
       "Supernatural",
     ]
-  }
+  },
+  {
+    numb: 2,
+    question: "What song is this lyric from: Every look, every touch Makes me wanna give you my heart",
+    answer: "Moonlight",
+    options: [
+      "Dangerous Women",
+      "Greedy",
+      "Moonlight",
+      "Into You",
+    ]
+  },
+  {
+    numb: 3,
+    question: "What song is this lyric from: Made my decision to test my limits",
+    answer: "Dangerous Women",
+    options: [
+      "Dangerous Women",
+      "Leave Me Lonely",
+      "Everyday",
+      "I Don't Care",
+    ]
+  },
+  {
+    numb: 4,
+    question: "What song is this lyric from: When finding love is a battle ",
+    answer: "Be Alright",
+    options: [
+      "Be Alright",
+      "Bad Decisions",
+      "Touch It",
+      "Moonlight",
+    ]
+  },
+  {
+    numb: 5,
+    question: "What song is this lyric from: So name a game to play",
+    answer: "Into You",
+    options: [
+      "Be Alright",
+      "Leave My Lonely",
+      "Into You",
+      "Sometimes",
+    ]
+  },
+  {
+    numb: 6,
+    question: "What song is this lyric from: I've been here all night",
+    answer: "Side To Side",
+    options: [
+      "Be Alright",
+      "I Don't Care",
+      "Step On Up",
+      "Side To Side",
+    ]
+  },
+  {
+    numb: 7,
+    question: "What song is this lyric from: I just broke up with my ex",
+    answer: "Let me Love You",
+    options: [
+      "Let me Love You",
+      "Bad Decisions",
+      "Jason's Song",
+      "Everyday",
+    ]
+  },
+  {
+    numb: 8,
+    question: "What song is this lyric from: You know that I'm coming tonight",
+    answer: "Greedy",
+    options: [
+      "Knew Better",
+      "Touch It",
+      "Greedy",
+      "Bad Decisions",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What song is this lyric from: You're no good for me, darling(ah)",
+    answer: "Leave My Lonely",
+    options: [
+      "Touch It",
+      "Jason Song",
+      "Leave My Lonely",
+      "Everyday",
+    ]
+  },
+  {
+    numb: 10,
+    question: "What song is this lyric from: All I want, all I need, honestly, it's just me and you",
+    answer: "Everday",
+    options: [
+      "Touch It",
+      "Bad Decisions",
+      "Thinking Bout You",
+      "Everyday",
+    ]
+  },
+  {
+    numb: 11,
+    question: "What song is this lyric from: I've never been so caught up",
+    answer: "Sometimes",
+    options: [
+      "Side To Side",
+      "Into You",
+      "Sometimes",
+      "Everyday",
+    ]
+  },
+  {
+    numb: 12,
+    question: "What song is this lyric from: That used to be important to me",
+    answer: "I Don't Care",
+    options: [
+      "Greedy",
+      "Side To Side",
+      "Be Alright",
+      "I Don't Care",
+    ]
+  },
 ]
