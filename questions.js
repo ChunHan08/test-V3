cddddfe5dcca5eb5972eb19d5a35c5f9c49a1058
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-20 17:05:20 +0000

    update

---
+++ questions.js
@@ -0,0 +1,13 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What song is this lyric from: Baby, would I still be your lover?",
+    answer: "Intro",
+    options: [
+      "Intro",
+      "Baby",
+      "yes,and",
+      "Supernatural",
+    ]
+  }
+]
